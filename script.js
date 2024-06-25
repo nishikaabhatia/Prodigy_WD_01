0cd00ddc2e396f8aae7f8b9c08b438e0a311fb0b
nishikaabhatia <nishikaabhatia@gmail.com>
2024-06-25 20:53:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+btn1= document.querySelector(".nav-cart");
+btn1.onmouseover= ()=>{
+    btn1.style.color="yellow";
+};
+btn2= document.querySelector(".nav-return");
+btn2.onmouseover= ()=>{
+    btn2.style.color="yellow";
+};
+btn3= document.querySelector(".nav-signin");
+btn3.onmouseover= ()=>{
+    btn3.style.color="yellow";
+};
+btn4= document.querySelector(".nav-address");
+btn4.onmouseover= ()=>{
+    btn4.style.color="yellow";
+};
